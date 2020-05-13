7b8a8a0c4b3bc4bdeb0863993cebb4d4d4cec7d5
Somnath Paul <Somnath4x@gmail.com>
2020-05-13 21:23:03 +0530

    modified: added preorder serch

--- tree/main.py
+++ tree/main.py
@@ -1,3 +1,10 @@
+
+#               1
+#           /       \  
+#          2          3  
+#         /  \      /   \
+#        4    5     6   7 
+
 # node of the tree
 class Node(object):
     def __init__(self, value):
@@ -9,3 +16,35 @@ class Node(object):
 class Tree(object):
     def __init__(self, root):
         self.root = Node(root)
+    
+    def print(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder(tree.root, "")
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False
+
+    def preorder(self, start, traversal):
+        # node visiting pattern
+        """Root->Left->Right"""
+
+        if start:
+            traversal = traversal + (str(start.value) + "-")
+            traversal = self.preorder(start.left, traversal)
+            traversal = self.preorder(start.right, traversal)
+
+        return traversal
+
+
+
+
+# Set up tree:
+tree = Tree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+print(tree.print("preorder"))
