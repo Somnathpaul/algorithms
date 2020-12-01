3d332acb4789549551b99bce0cc1c9edb942a274
Somnath Paul <Somnath4x@gmail.com>
2020-12-02 00:06:41 +0530

    modified

--- lcp/main.py
+++ lcp/main.py
@@ -2,23 +2,21 @@ def lcp(str):
     length_of_str = len(str[0])
     initial_word = str[0]
     for i in range(len(str)):
-        #print(len(str[i]))
         if len(str[i]) < length_of_str:
             length_of_str = len(str[i])
             initial_word = str[i]
 
-    #return length_of_str
+    #loop through through words leaving 1st one and check for every letter
     word = 0
     sub_str = ''
-    while word < len(str):
-        for i in str[word]:
-            for j in range(len(initial_word)):
-                if i == initial_word[j]:
-                    sub_str = sub_str + i
-                    print(sub_str)
-        word = word+1
-        print(word)
-    print("-")
+    while word < length_of_str:
+        char = str[0][word]
+        for j in range(1, len(str)):
+            if str[j][word] != char:
+                return sub_str
+        sub_str = sub_str + char
+        word = word +1
+    return sub_str
 
 str = ["flower","flow","flight"]
 print(lcp(str))
